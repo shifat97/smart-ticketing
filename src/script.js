1f43723cdc9a2832b69f61b353598240771a2a24
shifat97 <shifatbinreza2@gmail.com>
2024-04-08 00:20:08 +0600

    added header

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const nav3bar = document.getElementById('nav-3-bar');
+const nav3items = document.getElementById('nav-3-items');
+
+nav3bar.addEventListener('click', function () {
+    nav3items.classList.toggle('hidden');
+})
