50cfb5e4e4ca837bed7ed568ad792d7cf1cd3997
shifat97 <shifatbinreza2@gmail.com>
2024-04-09 20:33:34 +0600

    added feature: added calculation for total and grand total after getting discount

--- src/script.js
+++ src/script.js
@@ -5,10 +5,16 @@ const seats = document.getElementsByClassName('seat');
 const bookingListContainer = document.getElementById('booking-list-container');
 const seatCount = document.getElementById('seat-count');
 const totalPrice = document.getElementById('total-price');
+const grandTotal = document.getElementById('grand-total');
 const couponApplyButton = document.getElementById('coupon-apply-btn');
+const coupon1 = document.getElementById('coupon-1');
+const coupon2 = document.getElementById('coupon-2');
+const couponInputElement = document.getElementById('coupon');
+const couponInputContainer = document.getElementById('coupon-input-container');
+const discountPrice = document.getElementById('discount-price');
+const showDiscountPrice = document.getElementById('show-discount-price');
 
 let bookedSeatList = [];
-
 let maxBooking = 0;
 
 // Navigation menu for mobile version
@@ -47,7 +53,7 @@ function getSeatById(seatId) {
 // This function decrement total seat by 1.
 function totalSeatsLeft() {
     const totalSeat = parseInt(seatAvailable.textContent);
-    const remainSeat = totalSeat - 1;
+    const remainSeat = totalSeat - 1; // Decrease total seat by 1
     seatAvailable.textContent = remainSeat;
 }
 
@@ -55,7 +61,7 @@ function totalSeatsLeft() {
 // Shows total of seat booked
 function addToBookingList(seatId) {
     const totalSeatBooked = parseInt(seatCount.textContent) + 1; // Increase seat count by 1
-    seatCount.textContent = totalSeatBooked; 
+    seatCount.textContent = totalSeatBooked;
     const seat = `
     <tr>
         <td class="text-16 pt-2">${seatId}</td>
@@ -65,22 +71,65 @@ function addToBookingList(seatId) {
     `
     bookingListContainer.insertAdjacentHTML('beforeend', seat);
     calculateTotalPrice();
-    // checkEligibleForCoupon(totalSeatBooked);
+    checkEligibleForCoupon(totalSeatBooked);
 }
 
 // This function enables coupon apply button if user book 4 seats.
-// function checkEligibleForCoupon(totalSeatBooked) {
-//     if (totalSeatBooked === 4) {
-//         couponApplyButton.removeAttribute('disabled');
-//     } 
-// }
+function checkEligibleForCoupon(totalSeatBooked) {
+    if (totalSeatBooked === 4) {
+        couponApplyButton.removeAttribute('disabled');
+    }
+}
 
-// couponApplyButton.addEventListener('click', function () {
-//     console.log('Hello');
-// })
+// This event checks for coupon validity on apply click.
+// Calculates the total discount price.
+// Calculates grand total after getting discount price.
+couponApplyButton.addEventListener('click', function () {
+    const couponCode1 = coupon1.textContent;
+    const couponCode2 = coupon2.textContent;
+    const couponInputElementValue = couponInputElement.value;
+    const getGrandTotal = parseFloat(grandTotal.textContent);
+
+    let discount;
+    let grandTotalDiscount;
+
+    if (couponInputElementValue === '') { // Check if coupon field is empty
+        alert('Please enter your coupon code!');
+        return;
+    } else if (!((couponInputElementValue === couponCode1) || (couponInputElementValue === couponCode2))) { // Check if coupon code is valid or not
+        alert('Your coupon code is not valid!');
+        return;
+    } else if (couponInputElementValue === couponCode1) {
+        discount = calculateDiscount(15, getGrandTotal); // Calculates the total discount
+    } else if (couponInputElementValue === couponCode2) {
+        discount = calculateDiscount(20, getGrandTotal); // Calculates the total discount
+    }
+    const div = `
+            <div class="flex justify-between">
+                <p class="font-semibold">Discount</p>
+                <p class="text-right text-16 font-semibold">- <span id="discount-price">${discount}</span></p>
+            </div>
+        `
+    showDiscountPrice.insertAdjacentHTML('beforeend', div);
+
+    grandTotalDiscount = getGrandTotal - discount; // Subtract discount from grand total
+    setGranTotalPrice(grandTotalDiscount); // Add discounted grand total price
+    couponInputContainer.classList.add('hidden'); // Hide the coupon field after applying
+})
+
+// This function calculates the discount.
+function calculateDiscount(discountPercent, getGrandTotal) {
+    return ((discountPercent / 100) * getGrandTotal);
+}
 
 // This function calculates the total price.
 function calculateTotalPrice() {
     const currentTotal = parseFloat(totalPrice.textContent);
-    totalPrice.textContent = currentTotal + 550; // Increment total price by adding 550 each.
+    totalPrice.textContent = currentTotal + 550; // Increment total price by adding 550 each
+    setGranTotalPrice(totalPrice.textContent);
+}
+
+// This function calculates the grand total price.
+function setGranTotalPrice(grandTotalPrice) {
+    grandTotal.textContent = grandTotalPrice; // Add total price to grand total
 }
